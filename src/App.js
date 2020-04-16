d7754f46099cc0a6c121da3cd23d095d9eb15845
unknown <prakashbahadursingh2@gmail.com>
2020-04-16 02:03:25 -0700

    header created

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, {useState, useEffect} from "react";
 import "./App.css";
 import Axios from "axios";
+
+
+
+
 const api_key ='takBkO9usLbQrDWvPcvd3IKuviGcJ3xtkT5FP2Hb'
-const url = 'https://api.nasa.gov/planetary/apod?api_key=takBkO9usLbQrDWvPcvd3IKuviGcJ3xtkT5FP2Hb'
+const nasaURL = 'https://api.nasa.gov/planetary/apod?api_key=takBkO9usLbQrDWvPcvd3IKuviGcJ3xtkT5FP2Hb'
+const nasaLogo = 'https://www.stickpng.com/assets/images/58429400a6515b1e0ad75acc.png'
+const nasaSeal = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/NASA_seal.svg/1024px-NASA_seal.svg.png'
 
 
 function App() {
+const [imageData, setImageData] = useState(null);
+const [date, setDate] = useState('')
+
   //https://api.nasa.gov/planetary/apod?api_key=takBkO9usLbQrDWvPcvd3IKuviGcJ3xtkT5FP2Hb
   useEffect (() =>{
-    Axios.get(url)
+    Axios.get(nasaURL)
       .then(success => {
-        console.log(res)
+        console.log(success)
+        debugger
       })
       .catch (error => {
         console.log('error')
@@ -18,10 +28,32 @@ function App() {
   }, [])
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <header className = 'App-header'>
+        <div >
+          <img className= 'nasa-logo' src = {nasaLogo} ></img>
+        </div>
+        <div>
+          <h1>NASA Photo of the Day</h1>
+        </div>
+        <div >
+          <img className = 'nasa-seal' src = {nasaSeal}></img>
+        </div>
+      </header>
+      
+      {/* <ImageTitle/> */}
+      {/* <NasaImage/> */}
+      {/* <NasaDate/> */}
+      {/* NasaExplanation/> */}
+      {/* NasaCopyRight */}
+           <p>
+          Read through the instructions in the README.md file to build your NASA
+          app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        </p>
+        </footer>
+       
+
+
+     
     </div>
   );
 }
